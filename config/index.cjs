47d7919b3187cd0eb85d950c3ec8187c30a6827f
zyc0605 <54566263+zyc0605@users.noreply.github.com>
2025-01-06 17:51:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,10 +29,10 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '崽崽', year: '2000', date: '10-27',
+          type: '生日', name: '崽崽', year: '2000', date: '10-27'
         },
         {
-          type: '节日', name: '相识纪念日', year: '2023', date: '04-09',
+          type: '节日', name: '相识纪念日', year: '2023', date: '04-09'
         }
       ],
       // 我们在一起已经有xxxx天了的配置
